80ac703124d07cb97a80a1fb958b9847db5be81e
SomeRandomDeveloper <git@srd.dev>
2026-05-26 20:18:30 +0200

    Add github action for phpcs + phan

---
+++ .github/installWiki.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+MW_BRANCH=$1
+
+git clone -b $MW_BRANCH --depth 1 git@github.com:weirdgloop/mediawiki.git mediawiki
+cd mediawiki
+
+composer install
+
+git clone https://github.com/wikimedia/mediawiki-extensions-ContactPage.git extensions/ContactPage
+git clone https://github.com/weirdgloop/mediawiki-extensions-Scribunto.git extensions/Scribunto
+
+# Temporarily commented out since we don't run any unit tests right now
+: <<'COMMENT'
+php maintenance/install.php --dbtype sqlite --dbuser root --dbname mw --dbpath $(pwd) --pass AdminPassword WikiName AdminUser
+
+# echo 'error_reporting(E_ALL| E_STRICT);' >> LocalSettings.php
+# echo 'ini_set("display_errors", 1);' >> LocalSettings.php
+echo '$wgShowExceptionDetails = true;' >> LocalSettings.php
+echo '$wgShowDBErrorBacktrace = true;' >> LocalSettings.php
+echo '$wgDevelopmentWarnings = true;' >> LocalSettings.php
+
+echo 'wfLoadExtension( "GloopTweaks" );' >> LocalSettings.php
+
+cat <<EOT >> composer.local.json
+{
+  "require": {
+
+  },
+	"extra": {
+		"merge-plugin": {
+			"merge-dev": true,
+			"include": []
+		}
+	}
+}
+EOT
+
+# Download phpunit.xml.dist or phpunit.xml.template as they're not in the tarballs
+# Taken from https://github.com/StarCitizenTools/mediawiki-ci-workflows/blob/main/.github/workflows/test-php.yml
+wget "https://raw.githubusercontent.com/weirdgloop/mediawiki/${MW_BRANCH}/phpunit.xml.dist" -nv || \
+  wget "https://raw.githubusercontent.com/weirdgloop/mediawiki/${MW_BRANCH}/phpunit.xml.template" -nv
+COMMENT
